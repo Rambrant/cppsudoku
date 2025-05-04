7686815a2de31f0884ec7638d967fcb1d81a6298
Thomas Rambrant <thomas@rambrant.com>
2025-05-04 15:40:07 +0200

    Board parsed and constrained...

--- includes/ConstraintPropagationSolver.h
+++ includes/ConstraintPropagationSolver.h
@@ -3,10 +3,13 @@
 //
 #pragma once
 
-#include <ISudokuSolver.hpp>
 #include <map>
 #include <set>
 #include <utility>
+#include <optional>
+
+
+#include "ISudokuSolver.hpp"
 
 class ConstraintPropagationSolver : public ISudokuSolver
 {
@@ -16,7 +19,8 @@ public:
     using CellKey  = std::pair< int, int>;
     using CellList = std::set< CellKey>;
     using ValueMap = std::map< CellKey, std::vector<int>>;
-    using CellMap  = std::map< CellKey, CellList>;
+    using UnitMap  = std::map< CellKey, std::vector<CellList>>;
+    using PeerMap  = std::map< CellKey, CellList>;
 
     ConstraintPropagationSolver();
     ~ConstraintPropagationSolver() override = default;
@@ -27,13 +31,12 @@ private:
 
     mutable std::size_t mRecursions{};
 
-    mutable CellList mCellKeys{}; // All coordinates of the board cells
-    mutable ValueMap mValues{};   // The current possible values for a cell
-    mutable CellMap  mUnits{};    // All the cells in the same row, column and subsection as the cell
-    mutable CellMap  mPeers{};    // Same as mUnits but without the actual cell
+    CellList mCellKeys{}; // All coordinates of the board cells
+    UnitMap  mUnits{};    // All units, the cells in the row, column and subsection the cell belongs to
+    PeerMap  mPeers{};    // All the peers to the cell. mUnits flattened and without the actual cell
 
-    static auto createAllKeys( const Traits::BoardArray& rowRange, const Traits::BoardArray& colRange) -> CellList;
-    static auto createSubsectionKeys( const CellKey& key) -> CellList;
-
-    auto solver( Traits::Board& board) const -> bool;
+    auto parseGrid( const Traits::Board& board) const -> ValueMap;
+    auto assign( ValueMap& valuesMap, const CellKey& key, int value) const -> bool;
+    auto eliminate( ValueMap& valuesMap, const CellKey& key, int value) const -> bool;
+    auto search( ValueMap valuesMap) const -> ValueMap;
 };
--- src/ConstraintPropagationSolver.cpp
+++ src/ConstraintPropagationSolver.cpp
@@ -5,74 +5,166 @@
 
 #include "ConstraintPropagationSolver.h"
 
-template<typename RowRange, typename ColRange>
-static auto createKeys( const RowRange& rowRange, const ColRange& colRange) -> ConstraintPropagationSolver::CellList
-{
-    ConstraintPropagationSolver::CellList list;
-
-    for( auto row : rowRange)
-    {
-        for( auto col: colRange)
-        {
-            list.emplace( row, col);
-        }
-    }
-
-    return list;
-}
 
 ConstraintPropagationSolver::ConstraintPropagationSolver()
 {
-    mCellKeys = std::move( createKeys( Traits::INDEX_RANGE, Traits::INDEX_RANGE));
+    //
+    // Init all the cell keys
+    //
+    for( auto r : Traits::INDEX_RANGE)
+    {
+        for( auto c : Traits::INDEX_RANGE)
+        {
+            mCellKeys.emplace( r, c);
+        }
+    }
 
+    //
+    // For all existing keys init all units and peers
+    //
     for( auto key: mCellKeys)
     {
-        CellList rowKeys( createKeys( std::vector<int>{key.first}, Traits::INDEX_RANGE));
+        auto [rowIdx, colIdx] = key;
 
-        mUnits[ key].insert( rowKeys.begin(), rowKeys.end());
-        mPeers[ key].merge( rowKeys);
+        CellList rowUnit, colUnit, boxUnit;
 
-        CellList colKeys( createKeys( Traits::INDEX_RANGE, std::vector{key.second}));
+        for ( auto c : Traits::INDEX_RANGE)
+        {
+            rowUnit.emplace( rowIdx, c);
+            mPeers[key].emplace( rowIdx, c);
+        }
 
-        mUnits[ key].insert( colKeys.begin(), colKeys.end());
-        mPeers[ key].merge( colKeys);
+        for ( auto r : Traits::INDEX_RANGE)
+        {
+            colUnit.emplace( r, colIdx);
+            mPeers[key].emplace( r, colIdx);
+        }
 
-        CellList subKeys( createSubsectionKeys( key));
+        int boxRowStart = (rowIdx / Traits::SUBSECTION_SIZE) * Traits::SUBSECTION_SIZE;
+        int boxColStart = (colIdx / Traits::SUBSECTION_SIZE) * Traits::SUBSECTION_SIZE;
 
-        mUnits[ key].insert( subKeys.begin(), subKeys.end());
-        mPeers[ key].merge( subKeys);
+        for( int r = boxRowStart; r < boxRowStart + Traits::SUBSECTION_SIZE; ++r)
+        {
+            for( int c = boxColStart; c < boxColStart + Traits::SUBSECTION_SIZE; ++c)
+            {
+                boxUnit.emplace( r, c);
+                mPeers[key].emplace( r, c);
+            }
+        }
 
-        mPeers[key].erase( key);
+        mUnits[key] = { rowUnit, colUnit, boxUnit};
+        mPeers[key].erase( key);    // Remove the key from the peers
     }
 }
 
 auto ConstraintPropagationSolver::solve( Traits::Board &board) const -> Traits::BoardResult
 {
-    bool result = solver( board);
+    ValueMap values{ parseGrid( board)};
+
+    bool result = search( values);
 
     return std::make_tuple( result, mRecursions);
 }
 
-auto ConstraintPropagationSolver::createSubsectionKeys( const CellKey &key) -> CellList
+
+auto ConstraintPropagationSolver::search( ValueMap valuesMap) const -> ValueMap
 {
-    CellList list{};
+    return true;
+}
 
-    int rowStart = (key.first  / Traits::SUBSECTION_SIZE) * Traits::SUBSECTION_SIZE;
-    int colStart = (key.second / Traits::SUBSECTION_SIZE) * Traits::SUBSECTION_SIZE;
+auto ConstraintPropagationSolver::parseGrid( const Traits::Board & board) const -> ValueMap
+{
+    ValueMap values;
 
-    for( int rowIdx = 0; rowIdx < Traits::SUBSECTION_SIZE; ++rowIdx)
+    for( auto key : mCellKeys)
     {
-        for( int colIdx = 0; colIdx < Traits::SUBSECTION_SIZE; ++colIdx)
+        auto& vec = values[key];
+        vec.insert( vec.end(), Traits::VALUE_RANGE.begin(), Traits::VALUE_RANGE.end());
+    }
+
+    for( auto [ rowIdx, colIdx ] : mCellKeys)
+    {
+        auto value = board[ rowIdx][ colIdx];
+
+        if( value != Traits::NO_VALUE)
         {
-            list.emplace( rowStart + rowIdx, colStart + colIdx);
+            if( ! assign( values, { rowIdx, colIdx }, value))
+            {
+                std::string key = "[" + std::to_string( rowIdx) + "," + std::to_string( colIdx) + "]";
+                throw std::runtime_error("Illegal board: contradiction at [" + key + "] for digit " + std::to_string(value));
+            }
         }
     }
 
-    return list;
+    return values;
 }
 
-bool ConstraintPropagationSolver::solver(Traits::Board &board) const
+auto ConstraintPropagationSolver::assign( ValueMap& valuesMap, const CellKey& key, const int value) const -> bool
 {
-     return true;
+    //
+    // Collect other digits to eliminate
+    //
+    auto& cellValues = valuesMap[key];
+
+    std::vector<int> otherValues;
+    std::copy_if( cellValues.begin(), cellValues.end(), std::back_inserter( otherValues), [&](int d) { return d != value; });
+
+    //
+    // Eliminate all remaining digits
+    //
+    for( const auto otherValue : otherValues)
+    {
+        if( ! eliminate( valuesMap, key, otherValue))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+auto ConstraintPropagationSolver::eliminate( ValueMap& valuesMap, const CellKey& key, const int value) const -> bool
+{
+    auto& valList = valuesMap[key];
+
+    if( find( valList.begin(), valList.end(), value) == valList.end())
+        return true;
+
+    valList.erase( remove( valList.begin(), valList.end(), value), valList.end());
+
+    if( valList.empty()) return false;
+
+    if( valList.size() == 1)
+    {
+        int lastValue = valList[0];
+
+        for( const auto& peer : mPeers.at(key))
+        {
+            if( ! eliminate( valuesMap, peer, lastValue))
+                return false;
+        }
+    }
+
+    for( const auto& unit : mUnits.at(key))
+    {
+        std::vector<CellKey> places;
+
+        for( const auto& s : unit)
+        {
+            if( std::find( valuesMap[s].begin(), valuesMap[s].end(), value) != valuesMap[s].end())
+                places.push_back(s);
+        }
+
+        if( places.empty())
+            return false;
+
+        if( places.size() == 1)
+        {
+            if( ! assign( valuesMap, places[0], value))
+                return false;
+        }
+    }
+
+    return true;
 }
 
