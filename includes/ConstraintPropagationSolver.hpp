dd2542eedacce9dd3e984d42407b2ce0e17c8bef
Thomas Rambrant <thomas@rambrant.com>
2025-05-16 11:44:09 +0200

    Moved all private members of ConstraintPropagationSolver to the cpp file

--- includes/ConstraintPropagationSolver.hpp
+++ includes/ConstraintPropagationSolver.hpp
@@ -12,6 +12,8 @@
 
 namespace com::rambrant::sudoku
 {
+    using Traits = SudokuTraits;
+
     /**
      * @brief A Sudoku solver that uses a constraint propagation algorithm. Modeled after the idea from Peter Norvig, @see https://norvig.com/sudoku.html
      */
@@ -33,22 +35,5 @@ namespace com::rambrant::sudoku
              */
             [[nodiscard]]
             auto solve( Traits::Board& board) const -> Traits::BoardResult override;
-
-        private:
-
-            using Square       = std::pair< int, int>;                      // Coordinate of a single square on the Sudoku board.
-            using Squares      = std::set< Square>;                         // A set of unique square positions.
-            using SquareValues = std::map< Square, std::vector<int>>;       // Map from each square to its current list of possible values.
-            using Units        = std::map< Square, std::vector<Squares>>;   // Map from each square to its associated units (row, column, box).
-            using Peers        = std::map< Square, Squares>;                // Map from each square to its peers (other squares in its units, excluding itself).
-
-            Squares mSquares{};  // All squares on the board
-            Units   mUnits{};    // Units (row, column, section) each square belongs to.
-            Peers   mPeers{};    // Peers of each square (flattened units minus the square).
-
-            auto parseGrid( const Traits::Board& board) const -> SquareValues;
-            auto assign( SquareValues& allValues, const Square& square, int value) const -> bool;
-            auto eliminate( SquareValues& allValues, const Square& square, int value) const -> bool;
-            auto search( SquareValues allValues, int & recursions ) const -> SquareValues;
     };
 }
