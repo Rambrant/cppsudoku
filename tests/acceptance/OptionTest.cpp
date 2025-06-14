e40dce64aedca229835162b578643fd9cff873db
Thomas Rambrant <thomas@rambrant.com>
2025-06-14 09:19:19 +0200

    OptionTest parameterized

--- tests/acceptance/OptionTest.cpp
+++ tests/acceptance/OptionTest.cpp
@@ -4,9 +4,9 @@
 
 #include <regex>
 #include <catch2/catch_test_macros.hpp>
-#include <nlohmann/detail/input/parser.hpp>
 
 #include "SudokuTraits.hpp"
+#include "catch2/generators/catch_generators.hpp"
 
 using namespace com::rambrant::sudoku;
 
@@ -16,25 +16,13 @@ SCENARIO( "Sudoku help option [acceptance]")
 {
     CliRunner runner( SUDOKU_CLI_COMMAND, SUDOKU_CLI_WORKING_DIR);
 
-    GIVEN( "Help output with short flag")
+    std::string helpFlag = GENERATE( "-h", "--help");
+
+    GIVEN( "The flag: " + helpFlag)
     {
          WHEN( "The solver is run")
         {
-            const auto exitCode = runner.run( { "-h"});
-
-            THEN( "The output should be the help text")
-            {
-                CHECK( runner.outputContains( "Usage:"));
-                CHECK( exitCode == 0);
-            }
-        }
-    }
-
-    GIVEN( "Help output with long flag")
-    {
-        WHEN( "The solver is run")
-        {
-            const auto exitCode = runner.run( { "--help"});
+            const auto exitCode = runner.run( { helpFlag});
 
             THEN( "The output should be the help text")
             {
@@ -51,25 +39,13 @@ SCENARIO( "Sudoku quiet options [acceptance]")
     const std::string input_file = "tests/test-resources/board_simple.txt";
     std::regex        digitLineRegex( R"(^[ 0-9]+$)");
 
-    GIVEN( "Quiet output with short flag")
+    std::string quietFlag = GENERATE( "-q", "--quiet");
+
+    GIVEN( "The flag: " + quietFlag)
     {
         WHEN( "A solver is run in quiet mode")
         {
-            const auto exitCode = runner.run( { "-q", "-O", "line", "-s", "backtracking", "-i" , input_file});
-
-            THEN( "The output should only contain a line with the solver puzzle")
-            {
-                CHECK( std::regex_match( runner.output(), digitLineRegex));  // Only digits should be found
-                CHECK( exitCode == 0);
-            }
-        }
-    }
-
-    GIVEN( "Quiet output with long flag")
-    {
-        WHEN( "A solver is run in quiet mode")
-        {
-            const auto exitCode = runner.run( { "--quiet", "-O", "line", "-s", "backtracking", "-i" , input_file});
+            const auto exitCode = runner.run( { quietFlag, "-O", "line", "-s", "backtracking", "-i" , input_file});
 
             THEN( "The output should only contain a line with the solver puzzle")
             {
@@ -85,26 +61,13 @@ SCENARIO( "Sudoku verbose options [acceptance]")
     CliRunner         runner( SUDOKU_CLI_COMMAND, SUDOKU_CLI_WORKING_DIR);
     const std::string input_file = "tests/test-resources/board_simple.txt";
 
-    GIVEN( "Verbose output with short flag")
+    std::string verboseFlag = GENERATE( "-v", "--verbose");
+
+    GIVEN( "The flag: " + verboseFlag)
     {
         WHEN( "A solver is run in verbose mode")
         {
-            const auto exitCode = runner.run( { "-v", "-O", "line", "-s", "backtracking", "-i" , input_file});
-
-            THEN( "The output should contain extra information")
-            {
-                CHECK( runner.outputContains( "Initializing"));
-                CHECK( runner.outputContains( "...Logger"));
-                CHECK( exitCode == 0);
-            }
-        }
-    }
-
-    GIVEN( "Verbose output with long flag")
-    {
-        WHEN( "A solver is run in verbose mode")
-        {
-            const auto exitCode = runner.run( { "--verbose", "--output-format", "line", "-s", "backtracking", "-i" , input_file});
+            const auto exitCode = runner.run( { verboseFlag, "-O", "line", "-s", "backtracking", "-i" , input_file});
 
             THEN( "The output should contain extra information")
             {
--- tests/acceptance/SolverTest.cpp
+++ tests/acceptance/SolverTest.cpp
@@ -2,8 +2,8 @@
 // Created by Thomas Rambrant on 2025-06-03.
 //
 
+#include <regex>
 #include <catch2/catch_test_macros.hpp>
-#include <nlohmann/detail/input/parser.hpp>
 
 #include "SudokuTraits.hpp"
 
@@ -14,7 +14,7 @@ using Traits = SudokuTraits;
 
 SCENARIO( "Sudoku backtracking solver [acceptance]")
 {
-    CliRunner runner( SUDOKU_CLI_COMMAND, SUDOKU_CLI_WORKING_DIR);
+    CliRunner  runner( SUDOKU_CLI_COMMAND, SUDOKU_CLI_WORKING_DIR);
 
     GIVEN( "A valid Sudoku input file with an easy puzzle")
     {
@@ -22,11 +22,11 @@ SCENARIO( "Sudoku backtracking solver [acceptance]")
 
         WHEN( "The solver is run")
         {
-            const auto exitCode = runner.run( { "-q", "-s", "backtracking", "-i" , input_file});
+            const auto exitCode = runner.run( { "-v", "-s", "backtracking", "-i" , input_file});
 
             THEN( "The output should contain the solved puzzle")
             {
-                CHECK( runner.output().find('x') == std::string::npos);  // No unsolved cells should be found
+                CHECK( runner.outputContains( "[backtracking]"));
                 CHECK( exitCode == 0);
             }
         }
@@ -44,6 +44,44 @@ SCENARIO( "Sudoku backtracking solver [acceptance]")
             {
                 CHECK( runner.outputContains( "No solution"));
                 CHECK( exitCode == 1);
+            }
+        }
+    }
+}
+
+SCENARIO( "Sudoku constraint propagation solver [acceptance]")
+{
+    CliRunner  runner( SUDOKU_CLI_COMMAND, SUDOKU_CLI_WORKING_DIR);
+    std::regex digitLineRegex( R"(^[ 0-9]+$)");
+
+    GIVEN( "A valid Sudoku input file with an easy puzzle")
+    {
+        const std::string input_file = "tests/test-resources/board_simple.txt";
+
+        WHEN( "The solver is run")
+        {
+            const auto exitCode = runner.run( { "-q", "-O", "line", "-s", "constraint", "-i" , input_file});
+
+            THEN( "The output should contain the solved puzzle")
+            {
+                CHECK( std::regex_match( runner.output(), digitLineRegex));  // Only digits should be found
+                CHECK( exitCode == 0);
+            }
+        }
+    }
+
+    GIVEN( "A valid Sudoku input file with an hard puzzle")
+    {
+        const std::string input_file = "tests/test-resources/board_hard.txt";
+
+        WHEN( "The solver is run")
+        {
+            const auto exitCode = runner.run( { "--quiet", "-O", "line", "--solvers", "constraint", "--input" , input_file});
+
+            THEN( "No solution should be found")
+            {
+                CHECK( std::regex_match( runner.output(), digitLineRegex));  // Only digits should be found
+                CHECK( exitCode == 0);
             }
         }
     }
